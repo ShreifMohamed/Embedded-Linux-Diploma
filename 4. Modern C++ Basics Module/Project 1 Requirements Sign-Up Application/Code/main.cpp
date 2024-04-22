a93a57b67a072ceec7daade1a186e014db7f3f1e
Shreif Mohamed <60182811+ShreifMohamed@users.noreply.github.com>
2024-04-22 17:02:00 +0200

    Update main.cpp

--- 4. Modern C++ Basics Module/Project 1 Requirements Sign-Up Application/Code/main.cpp
+++ 4. Modern C++ Basics Module/Project 1 Requirements Sign-Up Application/Code/main.cpp
@@ -1,3 +1,12 @@
+/*
+    Project Requirements: Sign-Up Application
+    we're going to be writing a user registration application.
+    This will allow users to register with the system, providing both their name and age,
+    and we'll store this information in our own custom type.
+    We'll also provide the ability for a user to be looked up by an ID, retrieving their information.
+
+    Once you complete the activity, you should obtain an output similar to the following Figure GUI Project
+*/
 // Dependcy
 #include <iostream>
 #include <vector>
